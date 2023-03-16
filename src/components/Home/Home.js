72b70d30d23872c8868f855679813dc0303a6653
chris cook <chriscook13@hotmail.co.uk>
2023-03-16 20:38:59 +0000

    updated navbar with correct routing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 export default function Home() {
     return (
@@ -13,20 +15,19 @@ export default function Home() {
                 <div className='min-h-screen'>
                     <section>
                         <nav className='py-10 mb-20 flex justify-between'>
-                            <h1 className='text-2xl font-'>   DoddDigitals</h1>
+                            <h1 className='text-2xl font-'>  Pollen and Pollution</h1>
                             <ul className='flex items-center'>
                                 <li>
                                 </li>
-                                <li> <a className='bg-gray-800 text-white px-3 py-2 rounded-lg ml-2' href="/"> Home </a> </li>
-                                <li> <a className='bg-gray-600 text-white px-3 py-2 rounded-lg ml-2' href="/skills"> Skills </a> </li>
-                                <li> <a className='bg-gray-600 text-white px-3 py-2 rounded-lg ml-2' href="/projects"> Projects </a> </li>
-                                <li> <a className='bg-gray-600 text-white px-3 py-2 rounded-lg ml-2' href="/contact"> Contact </a> </li>
+                                <li> <NavLink className='bg-gray-800 text-white px-3 py-2 rounded-lg ml-2' to='/'>Home </NavLink></li>
+                                <li> <NavLink className='bg-gray-600 text-white px-3 py-2 rounded-lg ml-2' to='/pollen_types'>Pollen Types </NavLink></li>
+                                <li> <NavLink className='bg-gray-600 text-white px-3 py-2 rounded-lg ml-2' to='AQI'>Air Quality Index </NavLink>  </li>
                             </ul>
                         </nav>
 
                         <div className='text-center p-5'>
                             <h2 className='text-5xl py-2 text-gray-600 font-medium'>
-                                George's Portfolio Page
+                                Pollen and Pollution
                             </h2>
                             <h3 className='text-2xl py-2'>Designer, Creator and Crypto Enthusiast</h3>
                             <p className='text-md leading-5 text-gray-800 '>
