bfd6ddb23f6492b2fa25cd589145b9d338a149bb
G-Flodd <georgedodd@hotmail.co.uk>
2023-03-17 20:02:19 +0000

    Cards Added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -58,6 +58,67 @@ export default function Home() {
                             </div>
                         </div>
 
+                        <div class="flex justify-center">
+                            <div
+                                class="block max-w-sm rounded-lg bg-white text-center shadow-lg dark:bg-neutral-700 mx-5">
+                                <div
+                                    class="border-b-2 border-neutral-100 py-3 px-6 dark:border-neutral-600 dark:text-neutral-50">
+                                    Featured
+                                </div>
+                                <div class="p-6">
+                                    <h5
+                                        class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                                        Special title treatment
+                                    </h5>
+                                    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                                        With supporting text below as a natural lead-in to additional
+                                        content.
+                                    </p>
+                                    <button
+                                        type="button"
+                                        class="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                                        data-te-ripple-init
+                                        data-te-ripple-color="light">
+                                        Button
+                                    </button>
+                                </div>
+                                <div
+                                    class="border-t-2 border-neutral-100 py-3 px-6 dark:border-neutral-600 dark:text-neutral-50">
+                                    2 days ago
+                                </div>
+                            </div>
+
+                            <div
+                                class="block max-w-sm rounded-lg bg-white text-center shadow-lg dark:bg-neutral-700 mx-5">
+                                <div
+                                    class="border-b-2 border-neutral-100 py-3 px-6 dark:border-neutral-600 dark:text-neutral-50">
+                                    Featured
+                                </div>
+                                <div class="p-6">
+                                    <h5
+                                        class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                                        Special title treatment
+                                    </h5>
+                                    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                                        With supporting text below as a natural lead-in to additional
+                                        content.
+                                    </p>
+                                    <button
+                                        type="button"
+                                        class="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                                        data-te-ripple-init
+                                        data-te-ripple-color="light">
+                                        Button
+                                    </button>
+                                </div>
+                                <div
+                                    class="border-t-2 border-neutral-100 py-3 px-6 dark:border-neutral-600 dark:text-neutral-50">
+                                    2 days ago
+                                </div>
+                            </div>
+                        </div>
+
+
                         <div className='text-center p-5'>
                             <p className='text-md leading-5 text-gray-800 '>
 
