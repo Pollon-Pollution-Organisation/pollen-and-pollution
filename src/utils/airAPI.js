8278c50f45046e85297915a330b6b9234d16b5a2
LillyIlex <116085080+LillyIlex@users.noreply.github.com>
2023-03-17 19:18:49 +0000

    AirAPI data calls all working. Consts created to store all relevant responses. API keys stored in vars.

--- src/utils/airAPI.js
+++ src/utils/airAPI.js
@@ -1,24 +1,47 @@
 import axios from "axios";
 
+const lilKey = "6293641afdmshf1dda5cd5fe8c34p1388c5jsn3acc5d8ed8ba"
+const chrisKey = "326b80785amsh8543d3e232cdaa9p1b5362jsndeee73e28081"
+const georgeKey = ""
+const sophieKey = "01fc0c0291msh6c74688887e69bfp12b8f7jsnea920cfc819d"
 
 function AirAPI() {
 
 
-const options = {
-  method: 'GET',
-  url: 'https://air-quality.p.rapidapi.com/forecast/airquality',
-  params: {lat: '35.779', lon: '-78.638', hours: '72'},
-  headers: {
-    'X-RapidAPI-Key': '326b80785amsh8543d3e232cdaa9p1b5362jsndeee73e28081',
-    'X-RapidAPI-Host': 'air-quality.p.rapidapi.com'
-  }
-};
+    const options = {
+        method: 'GET',
+        url: 'https://air-quality.p.rapidapi.com/forecast/airquality',
+        params: { lat: '35.779', lon: '-78.638', hours: '72' }, //grab lat & lon to store in vars
+        headers: {
+            'X-RapidAPI-Key': sophieKey,
+            'X-RapidAPI-Host': 'air-quality.p.rapidapi.com'
+        }
+    };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+    axios.request(options).then(function (response) {
+        const airData = response.data;
+        console.log(airData);
+
+        const airQual = airData.data[0].aqi;
+        const carbonMonoxide = airData.data[0].co;
+        const nitrogenDioxide = airData.data[0].no2;
+        const ozone = airData.data[0].o3;
+        const sulfurDioxide = airData.data[0].so2;
+        const particles10 = airData.data[0].pm10;
+        const particles25 = airData.data[0].pm25;
+
+        console.log(airQual);
+        console.log(carbonMonoxide);
+        console.log(nitrogenDioxide);
+        console.log(ozone);
+        console.log(sulfurDioxide);
+        console.log(particles10);
+        console.log(particles25);
+
+      
+    }).catch(function (error) {
+        console.error(error);
+    });
 }
 
 export default AirAPI
--- src/utils/pollenAPI.js
+++ src/utils/pollenAPI.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 import { useState, useEffect } from 'react'
 
+const lilKey = "6293641afdmshf1dda5cd5fe8c34p1388c5jsn3acc5d8ed8ba"
+const chrisKey = "326b80785amsh8543d3e232cdaa9p1b5362jsndeee73e28081"
+const georgeKey = ""
+const sophieKey = "01fc0c0291msh6c74688887e69bfp12b8f7jsnea920cfc819d"
 
 export default function PollenAPI() {
 
@@ -9,20 +13,26 @@ export default function PollenAPI() {
             url: 'https://air-quality.p.rapidapi.com/current/airquality',
             params: { lon: '-73.00597', lat: '40.71427' },
             headers: {
-                'X-RapidAPI-Key': '326b80785amsh8543d3e232cdaa9p1b5362jsndeee73e28081',
+                'X-RapidAPI-Key': sophieKey,
                 'X-RapidAPI-Host': 'air-quality.p.rapidapi.com'
             }
         };
 
         axios.request(options).then(function (response) {
             console.log(options)
-            console.log(response.data);
-            //returning undefined
-            console.log(response.data.predominant_pollen_type);
-            console.log(response.data.pollen_level_tree);
-            console.log(response.data.pollen_level_grass);
-            console.log(response.data.pollen_level_weed);
-            console.log(response.data.mold_level);
+            const pollenData = response.data;
+          
+            const highestPollenType = pollenData.data[0].predominant_pollen_type
+            const treePollen = pollenData.data[0].pollen_level_tree
+            const grassPollen = pollenData.data[0].pollen_level_grass
+            const weedPollen = pollenData.data[0].pollen_level_weed
+            const moldLevel = pollenData.data[0].mold_level
+ 
+            console.log(highestPollenType);
+            console.log(treePollen);
+            console.log(grassPollen);
+            console.log(weedPollen);
+            console.log(moldLevel);
 
         }).catch(function (error) {
             console.error(error);
