6a0149704e6e88eedc2f5089431cfb6f9b81ded3
sophiemarisa <sophiecharlwood@live.co.uk>
2023-03-21 18:28:49 +0000

    editing pages and adding latLong api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 
 import { HashRouter as HashRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home/Home';
-import Header from './components/Header/Header';
-import Pollen from './components/PollenPage/PollenPage';
 import PollenAPI from './utils/pollenAPI';
 import AirAPI from './utils/airAPI';
-
-
-import Air from './components/AirPage/AirPage';
 import AQI from './components/AirQualityIndexPage/AQI';
 import PollenTypes from './components/PollenTypesPage/PollenTypesPage';
 import React from 'react';
-
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 
@@ -27,8 +21,6 @@ function App() {
           <Navbar/>
           <Routes>
             <Route exact path="/" element={<Home />} />
-            <Route exact path="/pollen" element={<Pollen />} />
-            <Route exact path="/air" element={<Air />} />
             <Route exact path="/AQI" element={<AQI />} />
             <Route exact path="/pollen_types" element={<PollenTypes />} />
           </Routes>
---
+++ src/utils/latLong.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useState, useEffect } from 'react'
+
+export function LatLongAPI() {
+
+    const sophKey = '01fc0c0291msh6c74688887e69bfp12b8f7jsnea920cfc819d'
+
+    const options = {
+        method: 'GET',
+        url: 'https://forward-reverse-geocoding.p.rapidapi.com/v1/forward',
+        params: {
+            city: 'london',
+            country: 'United Kingdom',
+            'accept-language': 'en',
+            polygon_threshold: '0.0'
+        },
+        headers: {
+            'X-RapidAPI-Key': sophKey,
+            'X-RapidAPI-Host': 'forward-reverse-geocoding.p.rapidapi.com'
+        }
+    };
+
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+        return response.data
+    }).catch(function (error) {
+        console.error(error);
+    });
+}
