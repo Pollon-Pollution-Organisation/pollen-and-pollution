6070ea3b5888560dcf7361f1aa72b24160c24de6
LillyIlex <116085080+LillyIlex@users.noreply.github.com>
2023-03-22 13:25:11 +0000

    Added if statments to pollen and air card.

--- src/components/Home/AirCard/AirCard.js
+++ src/components/Home/AirCard/AirCard.js
@@ -1,10 +1,34 @@
 
 import React from 'react';
+import { useState, useEffect } from 'react'
+import {AirAPI} from "../../../utils/airAPI";
+
+
 //return overall low-high air pollution OR good-bad air quality result with clickable card taking user to AirPage for each pullutants results
-function AirCard() {
+function AirCard(props) {
+    AirAPI(props.setairQualData)
+    
+    //getting error Air Data is not defined
+
+    const airLevel = (props) => {
+        if (airData.data[0].aqi < 50) {
+            return "GOOD"
+        } else if (airData.data[0].aqi < 100) {
+            return "MODERATE"
+        } else if (airData.data[0].aqi < 150) {
+            return "NOT GOOD"
+        } else if (airData.data[0].aqi < 200) {
+            return "BAD"
+        } else if (airData.data[0].aqi < 250 ) {
+            return "UNHEALTHY"
+        } else {
+            return 'HAZARDOUS'
+        }
+    }
+
     return (
         <div>
-
+    {airLevel()}
         </div>
     )
     
--- src/components/Home/PollenCard/PollenCard.js
+++ src/components/Home/PollenCard/PollenCard.js
@@ -1,13 +1,32 @@
 
 import React from 'react';
+import { useState, useEffect } from 'react'
+import {PollenAPI} from "../../../utils/pollenAPI";
+
 //return overall low-high, colour coded result with clickable card taking user to PollenPage for more detailed pollen type results
-function PollenCard() {
+
+function PollenCard(props) {
+
+ PollenAPI(props.setPollenData)
+//getting error Pollen Data is not defined
+    const pollenLevel = () => {
+        if (pollenData.data[0].mold_level == 1) {
+            return "LOW"
+        } else if (pollenData.data[0].mold_level == 2) {
+            return "MEDIUM"
+        } else if (pollenData.data[0].mold_level == 3) {
+            return "HIGH"
+        } else {
+            return 'VERY HIGH'
+        }
+    }
+
     return (
         <div>
-
+           {pollenLevel()}
         </div>
     )
-    
+
 };
 
 export default PollenCard
