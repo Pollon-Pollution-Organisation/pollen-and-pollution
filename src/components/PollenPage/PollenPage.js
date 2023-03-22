65bcbfe73e6706e83ac1a18c61b5fb39bf829f0d
sophiemarisa <sophiecharlwood@live.co.uk>
2023-03-22 20:10:46 +0000

    making buttons functional

---
+++ src/components/PollenPage/PollenPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//display tree, grass, weed levels
+//clickable cards to pollen type page
+
+function PollenPage() {
+    return (
+        <div>
+</div>
+    )
+    
+};
+
+export default PollenPage
